abbcdbbfe4598cf6a472da7a2aee26513c0b4eae
David Su <david.d.su@gmail.com>
2024-02-13 21:20:59 -0800

    Import audio; setup events + generate soundbanks; add listener

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,84 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_AMB_CITY_LP = 584628495U;
+        static const AkUniqueID PLAY_AMB_FOREST_LP = 149137005U;
+        static const AkUniqueID PLAY_AMB_INTERIOR_LP = 93749488U;
+        static const AkUniqueID PLAY_IMP_PICKUP = 3369882767U;
+        static const AkUniqueID PLAY_IMP_WALL = 2242885151U;
+        static const AkUniqueID PLAY_MUS_MAIN_LP = 4171924292U;
+        static const AkUniqueID PLAY_MVT_BALLROLLING_LP = 4193143717U;
+        static const AkUniqueID PLAY_UI_WIN = 2823495579U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace STA_GAMESTATE
+        {
+            static const AkUniqueID GROUP = 530992273U;
+
+            namespace STATE
+            {
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID STA_GAMESTATE_PLAYING = 1888728998U;
+                static const AkUniqueID STA_GAMESTATE_WIN = 2087516798U;
+            } // namespace STATE
+        } // namespace STA_GAMESTATE
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace SWI_CRATETYPE
+        {
+            static const AkUniqueID GROUP = 236169936U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID SWI_CRATETYPE_COMMON = 2649394644U;
+                static const AkUniqueID SWI_CRATETYPE_RARE = 1167673431U;
+                static const AkUniqueID SWI_CRATETYPE_UNCOMMON = 2693566681U;
+            } // namespace SWITCH
+        } // namespace SWI_CRATETYPE
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID RTPC_BALLSPEED = 3709966389U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAINSOUNDBANK = 534561221U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMB = 1117531639U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID SFX = 393239870U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
