9ed3008b7cfe5f4a22ecba633d27757738fd9526
David Su <david.d.su@gmail.com>
2024-02-19 22:43:08 -0800

    Implement bare-bones player footstep audio component, without switches (as an example only, will be reverted)

--- Assets/Scripts/Audio/PlayerFootstepAudioComponent.cs
+++ Assets/Scripts/Audio/PlayerFootstepAudioComponent.cs
@@ -10,46 +10,10 @@ public class PlayerFootstepAudioComponent : MonoBehaviour
     [SerializeField]
     AK.Wwise.Event footstepAudioEvent; // Audio event for footsteps
 
-    [SerializeField]
-    AK.Wwise.Switch footstepMaterialSwitch_Concrete; // Switcch for concrete
-
-    [SerializeField]
-    AK.Wwise.Switch footstepMaterialSwitch_Grass; // Switch for grass
-
-    Vector3 footstepRaycastStartPositionOffset = Vector3.up * 0.05f; // Footstep ray position offset
-    float footstepRaycastDistance = 100f; // How far to cast the footstep ray for
-
     // Handle footsteps (called by animation events)
     public void OnFootstep()
     {
-        // Cast a ray from the player position (with upwards offset) directly downwards
-        Vector3 startPosition = transform.position + footstepRaycastStartPositionOffset;
-        RaycastHit[] hits = Physics.RaycastAll(startPosition, Vector3.down, footstepRaycastDistance);
-
-        // Iterate through the raycast hits to see if we find any objects that match our surface tags
-        for (int i = 0; i < hits.Length; i++)
-        {
-            RaycastHit hit = hits[i];
-            Collider hitCollider = hit.collider;
-
-            // If we find a valid surface, we set the appropriate Wwise switch and break out of the loop
-            if (hitCollider.CompareTag("Concrete")) // Concrete
-            {
-                footstepMaterialSwitch_Concrete.SetValue(this.gameObject);
-                Debug.Log("OnFootstep(): Concrete");
-
-                break;
-            }
-            else if (hitCollider.CompareTag("Grass")) // Grass
-            {
-                footstepMaterialSwitch_Grass.SetValue(this.gameObject);
-                Debug.Log("OnFootstep(): Grass");
-
-                break;
-            }
-        }
-
-        // Finally, we post the footstep event to actually play a footstep sound
+        // Post the Wwise event corresponding to footstep audio
         footstepAudioEvent.Post(this.gameObject);
     }
 }
